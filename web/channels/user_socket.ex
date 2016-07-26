e066ebee65d7cbb755ea92e961f7394c19e226fd
Philip Hagen <phil@philiphagen.com>
2016-07-26 13:32:44 +0800

    Decrease websocket timeout to below Heroku's 55 sec window

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule PhoenixTestApp.UserSocket do
   # channel "room:*", PhoenixTestApp.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
